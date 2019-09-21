4d596190a166e5d86f53876e78abe663a0daed85
Steve Herrick <stephen.j.herrick@gmail.com>
2019-09-21 16:59:11 -0400

    Chapter 11

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import classes from './Modal.module.css';
-import Aux from '../../../hoc/Auxiliary';
+import Aux from '../../../hoc/Auxiliary/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
 
 const Modal = (props) => (
